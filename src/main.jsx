ee44ab19bd38638f10aec0a213ad7d46f9570395
0xvidejk <0xvidejk@gmail.com>
2023-12-20 23:27:58 +0300

    chore: remove App.jsx & add providers to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "normalize.css";
+import { Provider } from "react-redux";
+import { RouterProvider } from "react-router-dom";
+import router from "routes";
+import { store } from "store";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+import "styles/index.css";
+
+ReactDOM.createRoot(document.getElementById("app")).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>,
 );
